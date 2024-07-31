44aaf74e5d2b06225d013b2d3be9568f152dcb1f
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-07-31 09:08:32 -0600

    Funcion de registrar receta

--- Actividad recetas/Program.cs
+++ Actividad recetas/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Actividad_recetas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int option;
+            bool menu = true;
+            List<Recipe> recipesList = new List<Recipe>();
+            while (menu)
+            {
+                Console.Clear();
+                ShowMenu();
+                option= Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        {
+                            Console.Clear ();
+                            Recipe.AddRecipes(recipesList);
+                            Console.ReadKey();
+                            break;
+                        }
+                }
+                
+            }
+        }
+        public static void ShowMenu()
+        {
+            Console.WriteLine("1. Agregar receta.");
+        }
+    }
+}
--- Actividad recetas/Recipe.cs
+++ Actividad recetas/Recipe.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +20,20 @@ namespace Actividad_recetas
             Name = name;
             Ingredients = ingredients;
             Description = description;
+        }
+        public static void AddRecipes (List<Recipe> recipesList)
+        {
+            Console.WriteLine("Ingresa el número de receta.");
+            int id= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa el nombre de la receta.");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ingresa los ingredientes.");
+            string ingredients = Console.ReadLine();
+            Console.WriteLine("Ingresa la descripción o pasos de la receta.");
+            string description = Console.ReadLine();
+            Recipe recipe= new Recipe(id, name, ingredients, description);
+            recipesList.Add(recipe);
+            Console.WriteLine("La receta se ha registrado.");
         }
     }
 }
