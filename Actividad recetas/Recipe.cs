c8f82ddd69c586c26692ae00a9c12d6596701016
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-07-31 09:01:16 -0600

    Clase "Recipes" con metodo constructor  y clase "Program" con lista

---
+++ Actividad recetas/Recipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_recetas
+{
+    internal class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+        public string Description { get; set; }
+
+        public Recipe(int id, string name, string ingredients, string description)
+        {
+            Id = id;
+            Name = name;
+            Ingredients = ingredients;
+            Description = description;
+        }
+    }
+}
